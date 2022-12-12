b95d3a8de99a702ab5ba9ffc58691dcf5377ef4d
byeon2261 <114720002+byeon2261@users.noreply.github.com>
2022-12-12 14:38:28 +0900

    html_css Created

---
+++ Learning CSS/index.js
@@ -0,0 +1,12 @@
+const fix = document.querySelector("#fix");
+const fiximg = document.getElementById("fix");
+
+fiximg.style.backgroundImage = 'img/3.jpg';
+
+// const statusButton = document.querySelector("#status-button");
+
+// function handleStatusButton() {
+//     statusButton.innerText = "진짜 누르냐..";
+// }
+
+// statusButton.addEventListener("click", handleStatusButton);
